6a42306711bd5e8f6c998f0ab3256e0292b1c080
Hüseyin uyanık <93749348+huseyinuyanik@users.noreply.github.com>
2022-11-11 16:45:30 +0300

    Add files via upload

---
+++ hello/hk-hello-world/src/main/java/com/haydikodlayalim/hkhelloworld/HelloApi.java
@@ -0,0 +1,18 @@
+package com.haydikodlayalim.hkhelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class HelloApi {
+	
+	@GetMapping
+	public String hello() {
+		return "HelloWorld";
+	}
+	
+	
+
+}
